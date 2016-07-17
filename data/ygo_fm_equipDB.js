2935e445710cd7e651985a51feb45f88f964843a
Teddy Sudol <magesolumin@gmail.com>
2016-07-17 17:31:51 -0400

    Monster + equipment fusion data

---
+++ data/ygo_fm_equipDB.js
@@ -0,0 +1,757 @@
+var equipDB = TAFFY([
+  {
+    "left": "Air Marmot of Nefariousness",
+    "right": "Axe of Despair",
+    "type": "Equippable"
+  },
+  {
+    "left": "Amazon of the Seas",
+    "right": "Amphibious Bugroth (1850/1300)",
+    "type": "Equippable"
+  },
+  {
+    "left": "Ancient Jar",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Aqua Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Aqua Madoor",
+    "right": "Book of Secret Arts",
+    "type": "Equippable"
+  },
+  {
+    "left": "Armored Zombie",
+    "right": "Axe of Despair",
+    "type": "Equippable"
+  },
+  {
+    "left": "Baby Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Baby Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Battle Steer",
+    "right": "Kunai with Chain",
+    "type": "Equippable"
+  },
+  {
+    "left": "Black Dragon Jungle King",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Black Dragon Jungle King",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Black Dragon Jungle King",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Blackland Fire Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Blackland Fire Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Bolt Escargot",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Bolt Escargot",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Celtic Guardian",
+    "right": "Kunai with Chain",
+    "type": "Equippable"
+  },
+  {
+    "left": "Corroding Shark",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Crawling Dragon #2",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Crawling Dragon #2",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Curse of Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Curse of Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Curse of Dragon",
+    "right": "Horn of Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Cyber Saurus",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "D. Human",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dark Elf",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dark Elf",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dark Elf",
+    "right": "Electro-Whip",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dark Elf",
+    "right": "Elf's Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dark Elf",
+    "right": "Malevolent Nuzzler",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dark King of the Abyss",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dark Magician",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dark Magician",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Darkfire Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Darkfire Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Darkworld Thorns",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Deep Sea Shark",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Doma the Angel of Silence",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dragon Seeker",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dragon Zombie",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Dragoness the Wicked Knight",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Electric Snake",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Fire Reaper",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Firewing Pegasus",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Flame Swordsman",
+    "right": "Kunai with Chain",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gaia the Dragon Champion",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gaia the Fierce Knight",
+    "right": "Kunai with Chain",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gaia the Fierce Knight",
+    "right": "Sword of Dark Destruction",
+    "type": "Equippable"
+  },
+  {
+    "left": "Garoozis",
+    "right": "Kunai with Chain",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gate Guardian",
+    "right": "Sword of Destruction",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gemini Elf",
+    "right": "Book of Secret Arts",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gemini Elf",
+    "right": "Electro-Whip",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gemini Elf",
+    "right": "Elf's Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gemini Elf",
+    "right": "Malevolent Nuzzler",
+    "type": "Equippable"
+  },
+  {
+    "left": "Giltia the D.Kinght",
+    "right": "Kunai with Chain",
+    "type": "Equippable"
+  },
+  {
+    "left": "Gorgon Egg",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Great Mammoth of Goldfine",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Harpie's Pet Dragon",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Harpie's Pet Dragon",
+    "right": "Follow Wind",
+    "type": "Equippable"
+  },
+  {
+    "left": "Hercules Beetle",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Hibikime",
+    "right": "Electro-Whip",
+    "type": "Equippable"
+  },
+  {
+    "left": "Ice Water",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Ice Water",
+    "right": "Power of Kaishin",
+    "type": "Equippable"
+  },
+  {
+    "left": "Javelin Beetle Pact",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Judge Man",
+    "right": "Legendary Sword",
+    "type": "Equippable"
+  },
+  {
+    "left": "Kairyu-Shin",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Kairyu-Shin",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Kaiser Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Kaiser Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Kaminarikozou",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Key Mace",
+    "right": "Elf's Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Koumori Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Labyrinth Wall",
+    "right": "Invigoration",
+    "type": "Equippable"
+  },
+  {
+    "left": "Lesser Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Lesser Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Maiden of the Moonlight",
+    "right": "Elf's Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mammoth Graveyard",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Masked Clown",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Metal Guardian",
+    "right": "Axe of Despair",
+    "type": "Equippable"
+  },
+  {
+    "left": "Meteor Black Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Meteor Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Meteor Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Millennium Golem",
+    "right": "Axe of Despair",
+    "type": "Equippable"
+  },
+  {
+    "left": "Millennium Shield",
+    "right": "Legendary Sword",
+    "type": "Equippable"
+  },
+  {
+    "left": "Musician King",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mystic Horseman",
+    "right": "Electro-Whip",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mystical Elf",
+    "right": "Book of Secret Arts",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mystical Sand",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mystical Sand",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mystical Sand",
+    "right": "Electro-Whip",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mystical Sand",
+    "right": "Elf's Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mystical Sand",
+    "right": "Invigoration",
+    "type": "Equippable"
+  },
+  {
+    "left": "Mystical Sand",
+    "right": "Malevolent Nuzzler",
+    "type": "Equippable"
+  },
+  {
+    "left": "Nekogal #2",
+    "right": "Axe of Despair",
+    "type": "Equippable"
+  },
+  {
+    "left": "Nekogal #2",
+    "right": "Elf's Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "One-Eyed Shield Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "One-Eyed Shield Dragon",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "One-Eyed Shield Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Oscillo Hero #2",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Oscillo Hero #2",
+    "right": "Electro-Whip",
+    "type": "Equippable"
+  },
+  {
+    "left": "Petit Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Petit Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Pumpking the King of Ghosts",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Punished Eagle",
+    "right": "Follow Wind",
+    "type": "Equippable"
+  },
+  {
+    "left": "Queen of Autumn Leaves",
+    "right": "Elf's Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Rabid Horseman",
+    "right": "Legendary Sword",
+    "type": "Equippable"
+  },
+  {
+    "left": "Ray & Temperature",
+    "right": "Elf's Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Rogue Doll",
+    "right": "Book of Secret Arts",
+    "type": "Equippable"
+  },
+  {
+    "left": "Rose Spectre of Dunn",
+    "right": "Electro-Whip",
+    "type": "Equippable"
+  },
+  {
+    "left": "Seiyaryu",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Skelgon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Skelgon",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Skull Knight",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Skull Knight",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Skull Knight",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Skull Knight",
+    "right": "Kunai with Chain",
+    "type": "Equippable"
+  },
+  {
+    "left": "Skull Knight",
+    "right": "Malevolent Nuzzler",
+    "type": "Equippable"
+  },
+  {
+    "left": "Skull Knight",
+    "right": "Sword of Dark Destruction",
+    "type": "Equippable"
+  },
+  {
+    "left": "Solitude",
+    "right": "Axe of Despair",
+    "type": "Equippable"
+  },
+  {
+    "left": "Solitude",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Soul Hunters",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Soul Hunters",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Soul Hunters",
+    "right": "Malevolent Nuzzler",
+    "type": "Equippable"
+  },
+  {
+    "left": "Spirit of the Mountain",
+    "right": "Book of Secret Arts",
+    "type": "Equippable"
+  },
+  {
+    "left": "Steel Scorpion",
+    "right": "Machine Conversion Factory",
+    "type": "Equippable"
+  },
+  {
+    "left": "Stone Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Stone Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Suijin",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Summoned Skull",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Summoned Skull",
+    "right": "Black Pendant",
+    "type": "Equippable"
+  },
+  {
+    "left": "Summoned Skull",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Summoned Skull",
+    "right": "Horn of the Unicorn",
+    "type": "Equippable"
+  },
+  {
+    "left": "Summoned Skull",
+    "right": "Malevolent Nuzzler",
+    "type": "Equippable"
+  },
+  {
+    "left": "Summoned Skull",
+    "right": "Sword of Dark Destruction",
+    "type": "Equippable"
+  },
+  {
+    "left": "Swamp Battleguard",
+    "right": "Kunai with Chain",
+    "type": "Equippable"
+  },
+  {
+    "left": "Sword Arm of Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Sword Arm of Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Swordstalker",
+    "right": "Sword of Dark Destruction",
+    "type": "Equippable"
+  },
+  {
+    "left": "Terra the Terrible",
+    "right": "Axe of Despair",
+    "type": "Equippable"
+  },
+  {
+    "left": "The Wandering Doomed",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Thunder Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Thunder Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Tripwire Beast",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Turu-Purun",
+    "right": "Power of Kaishin",
+    "type": "Equippable"
+  },
+  {
+    "left": "Twin-Headed Thunder Dragon",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Twin-Headed Thunder Dragon",
+    "right": "Dark Energy",
+    "type": "Equippable"
+  },
+  {
+    "left": "Twin-Headed Thunder Dragon",
+    "right": "Dragon Treasure",
+    "type": "Equippable"
+  },
+  {
+    "left": "Twin-Headed Thunder Dragon",
+    "right": "Horn of Light",
+    "type": "Equippable"
+  },
+  {
+    "left": "Twin-Headed Thunder Dragon",
+    "right": "Invigoration",
+    "type": "Equippable"
+  },
+  {
+    "left": "Two-Headed King Rex",
+    "right": "Beast Fangs",
+    "type": "Equippable"
+  },
+  {
+    "left": "Warrior of Tradition",
+    "right": "Electro-Whip",
+    "type": "Equippable"
+  }
+])
