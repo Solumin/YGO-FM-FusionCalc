78aaf85caed573637486fe7e96a954a0aa875b83
Teddy Sudol <magesolumin@gmail.com>
2016-07-18 10:33:19 -0400

    Card DB now CSV and JSON
    - It's much easier to work with the raw data as a CSV
    - Of course it's even easier to work with it as a JSON file
    - Bugfix: "Kuwagata α" to "Kuwagata alpha"

--- scripts/make_carddb.rb
+++ scripts/make_carddb.rb
@@ -6,36 +6,24 @@ nonmons = ["Equip", "Field", "Magic", "Ritual", "Trap"]
 stars = ["Moon", "Mercury", "Sun", "Venus",
          "Jupiter", "Mars", "Neptune", "Pluto", "Saturn", "Uranus"]
 
-File.readlines("data/ygo_fm_cardDB.txt").each do |line|
-    fields = line.split
-    props = {}
+File.readlines("data/ygo_fm_cardDB.csv").each do |line|
+    fields = line.strip.split ";"
+    props = {:number => fields[0], :name => fields[1], :cardtype => fields[2]}
     # rindex in case the card has "Monster" in the name
-    if fields.rindex "Monster"
-        ctindex = fields.rindex "Monster" # index of the card type field
-        # OK the following line is a bit weird: We're looking for the first
-        # Guardian Star field, because the Monster Type can be multiple words.
-        # fields.index could fail if the name contains a star, e.g. "Moon Envoy"
-        sindex = fields.rindex {|x| stars.include? x} - 1 # index of star1 field
-        puts fields if sindex.nil?
-        props[:number] = fields[0]
-        props[:name] = fields[1,ctindex-1].join(" ")
-        props[:cardtype] = fields[ctindex]
-        props[:type] = fields[ctindex+1,sindex-(ctindex+1)].join(" ")
-        props[:star1] = fields[sindex]
-        props[:star2] = fields[sindex+1]
-        props[:level] = fields[sindex+2].to_i
-        props[:attack] = fields[sindex+3].to_i
-        props[:defense] = fields[sindex+4].to_i
+    if fields[2] == "Monster"
+        props[:type] = fields[3]
+        props[:star1] = fields[4]
+        props[:star2] = fields[5]
+        props[:level] = fields[6].to_i
+        props[:attack] = fields[7].to_i
+        props[:defense] = fields[8].to_i
         # These will be nil if the card doesn't have them
-        props[:password] = fields[sindex+5]
-        props[:cost] = fields[sindex+6]
+        props[:password] = fields[9]
+        props[:cost] = fields[10]
     else # other card types
-        ctindex = fields.rindex{|x| nonmons.include? x}
-        props[:number] = fields[0]
-        props[:name] = fields[1,ctindex-1].join(" ")
-        props[:cardtype] = fields[ctindex]
-        props[:password] = fields[ctindex+1]
-        props[:cost] = fields[ctindex+2]
+        # These will be nil if the card doesn't have them
+        props[:password] = fields[3]
+        props[:cost] = fields[4]
     end
     cards << props
 end
@@ -46,4 +34,8 @@ File.open("data/ygo_fm_cardDB.js", "w") {|file|
     file.write("var cardDB = TAFFY(#{JSON.pretty_generate(cards)})")
 }
 
-puts "Wrote JSON DB js file"
+File.open("data/ygo_fm_cardDB.json", "w") {|file|
+    file.write(JSON.pretty_generate(cards))
+}
+
+puts "Wrote JS and JSON files"
